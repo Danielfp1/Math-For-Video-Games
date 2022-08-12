0d5c3e08349d7af5e7dc1ceaf9884189a2442bb8
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-08-12 15:40:40 -0300

    Add name and icon to main window

---
+++ Home.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Math_For_Video_Games
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.label_home = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_home
+            // 
+            this.label_home.AutoSize = true;
+            this.label_home.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_home.Location = new System.Drawing.Point(365, 37);
+            this.label_home.Name = "label_home";
+            this.label_home.Size = new System.Drawing.Size(86, 31);
+            this.label_home.TabIndex = 0;
+            this.label_home.Text = "Home";
+            this.label_home.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_home);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Home";
+            this.Text = "Math For Video Games";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_home;
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Math_For_Video_Games
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
